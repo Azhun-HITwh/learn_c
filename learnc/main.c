d371b20ee928354493f36dc3194fbd2fe13dea41
Adrian.ZHU <zhuazhun1996@gmail.com>
2021-10-19 22:31:07 +0800

    Initial Commit

---
+++ learnc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  learnc
+//
+//  Created by Azhun Zhu on 2021/10/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
